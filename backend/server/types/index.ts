e93a3097c11bbb79009fce9f36d944612dcbef69
Houston <houstonhvrd@gmail.com>
2024-08-20 21:14:51 -0400

    backend removed

--- backend/server/types/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './auth.types';
-export * from './billing.types';
-export * from './maintenance.types';
-export * from './printer.types';
-export * from './tracking.types';
--- backend/server/types/maintenance.types.ts
+++
@@ -1,9 +0,0 @@
-export type MaintenanceRequest = {
-  id: string;
-  department_id: string;
-  printer_id: string;
-  request_date: string;
-  maintenance_type: string;
-  description: string;
-  status: string;
-};
