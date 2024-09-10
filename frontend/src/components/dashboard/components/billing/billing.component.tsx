1eb499b55cef27e6cf30f5dd6da9c75c63f9a723
Houston <houstonhvrd@gmail.com>
2024-09-10 17:28:49 -0400

    converted billing components to function components

--- frontend/src/components/dashboard/components/billing/billing.component.tsx
+++ frontend/src/components/dashboard/components/billing/billing.component.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ReactNode } from 'react';
+import React, { Component, ReactNode, useEffect } from 'react';
 import { ConnectedProps, connect } from 'react-redux';
 import { AppState } from '../../../../types';
 import {
@@ -8,79 +8,8 @@ import {
 import { Card, CardBody, CardHeader, Stack, Table } from 'react-bootstrap';
 import { departmentsMap } from '../../../../config/app-data';
 
-interface State {}
-
 interface Props {
   department?: string;
-}
-
-class BillingComponent extends Component<BillingComponentProps, State> {
-  async componentDidMount() {
-    this.getBillingInfo();
-  }
-
-  componentDidUpdate(
-    prevProps: Readonly<BillingComponentProps>,
-    prevState: Readonly<State>,
-    snapshot?: any
-  ): void {
-    if (prevProps.department !== this.props.department) {
-      this.getBillingInfo();
-    }
-  }
-
-  getBillingInfo = () => {
-    const { department } = this.props;
-    if (department) {
-      this.props.getCurrentInvoice(department);
-      this.props.getDepartmentInvoiceHistory(department);
-    } else {
-      this.props.getDepartmentInvoiceHistory();
-    }
-  };
-
-  render(): ReactNode {
-    const {
-      billing: { invoiceHistory },
-    } = this.props;
-    return (
-      <div data-testid='billing-component'>
-        <Stack gap={3}>
-          <Card>
-            <CardHeader>
-              <h2>Billing</h2>
-            </CardHeader>
-          </Card>
-          <Card>
-            <CardBody>
-              <Table>
-                <tbody>
-                  <tr>
-                    <th>Department</th>
-                    <th>Billing Period</th>
-                    <th>B&W Charges</th>
-                    <th>Color Charges</th>
-                    <th>Paper Charge</th>
-                    <th>Status</th>
-                  </tr>
-                  {invoiceHistory?.map((invoice) => (
-                    <tr key={invoice.id}>
-                      <td>{departmentsMap[invoice?.department_id]?.name}</td>
-                      <td>{invoice?.month + ' ' + invoice?.year}</td>
-                      <td>{invoice?.bw_charge}</td>
-                      <td>{invoice?.color_charge}</td>
-                      <td>{invoice?.paper_charge}</td>
-                      <td>{invoice?.status}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
-            </CardBody>
-          </Card>
-        </Stack>
-      </div>
-    );
-  }
 }
 
 const mapStateToProps = (state: AppState) => {
@@ -98,4 +27,61 @@ const connector = connect(mapStateToProps, mapDispatchToProps);
 
 type BillingComponentProps = ConnectedProps<typeof connector> & Props;
 
+const BillingComponent = (props: BillingComponentProps) => {
+  const {
+    billing: { invoiceHistory },
+  } = props;
+
+  useEffect(() => {
+    getBillingInfo();
+  }, [props.department]);
+
+  const getBillingInfo = async () => {
+    const { department } = props;
+    if (department) {
+      await props.getCurrentInvoice(department);
+      await props.getDepartmentInvoiceHistory(department);
+    } else {
+      await props.getDepartmentInvoiceHistory();
+    }
+  };
+
+  return (
+    <div data-testid='billing-component'>
+      <Stack gap={3}>
+        <Card>
+          <CardHeader>
+            <h2>Billing</h2>
+          </CardHeader>
+        </Card>
+        <Card>
+          <CardBody>
+            <Table>
+              <tbody>
+                <tr>
+                  <th>Department</th>
+                  <th>Billing Period</th>
+                  <th>B&W Charges</th>
+                  <th>Color Charges</th>
+                  <th>Paper Charge</th>
+                  <th>Status</th>
+                </tr>
+                {invoiceHistory?.map((invoice) => (
+                  <tr key={invoice.id}>
+                    <td>{departmentsMap[invoice?.department_id]?.name}</td>
+                    <td>{invoice?.month + ' ' + invoice?.year}</td>
+                    <td>{invoice?.bw_charge}</td>
+                    <td>{invoice?.color_charge}</td>
+                    <td>{invoice?.paper_charge}</td>
+                    <td>{invoice?.status}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </CardBody>
+        </Card>
+      </Stack>
+    </div>
+  );
+};
 export default connector(BillingComponent);
